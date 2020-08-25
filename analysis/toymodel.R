269d1c90bc952df6100f63356b3b3e70ee8e8ec4
nick-fournier <nichfournier@gmail.com>
2020-08-25 09:06:25 -0700

    Revised toy model

--- analysis/toymodel.R
+++ analysis/toymodel.R
@@ -11,6 +11,7 @@ fun.price <- function(k, pmin, pmax, a, b)  pmin + (pmax-pmin)/(1 + exp(a-b*k))
 
 #Elasticity function
 fun.elasticdemand <- function(k,p1,p2,E) k+k*(exp(-E*(p2-p1)/(p2+p1))-1)
+#fun.elasticdemand <- function(k,p1,p2,E) k*((p2-p1)/p1)^-E
 
 #Demand distribution (Manually constructed for this example)
 fun.demanddist <- function(scale, t,sd) scale*(dnorm(t,mean=9,sd) + dnorm(t,mean=17,sd) + dnorm(t,mean=12,sd=4) + dnorm(t,mean=14,sd=4))
@@ -60,7 +61,7 @@ dat[ , lapply(.SD, sum),.SDcols = c("n1","n2")]
 
 #### Run with varying pmax and elasticity ####
 #Set up combination matrix
-datvar = as.data.table(expand.grid(tval = t, E = seq(0,1, by=0.01), surge = seq(1, 3, by = 0.01)))
+datvar = as.data.table(expand.grid(tval = t, E = seq(0,2, by=0.025), surge = seq(1, 3, by = 0.025)))
 #Assign fixed price & scaled price
 datvar[ , price1 := pfix]
 datvar[ , pmax := pfix*surge]
@@ -102,6 +103,8 @@ revmat <- datvar[ , list("revratio" = sum(rev2)/sum(rev1)), by = .(E,surge)]
 
 #### Ploting ####
 
+
+#### Kernal Density 
 #rand = data.table(rand = c(rlnorm(50, sd=0.5), rnorm(50, mean=3, sd=0.5)))
 #rand = data.table(rand = rlnorm(5))
 rand = data.table(rand = c(-2.1,-1.3,-0.4,1.9,5.1,6.2))
@@ -118,6 +121,26 @@ plot.kernel <- ggplot(rand, aes(rand)) +
 for(x in rand$rand) plot.kernel <- plot.kernel + stat_function(fun = dnorm, args = list(mean = x, sd = d$bw), linetype=2)
 # plot.kernel
 
+
+#### Elasticity Plot
+elabs <- c(paste0("E = ",E), paste0("E = ",E/2), paste0("E = ",2*E))
+
+plot.elasticity <- ggplot(data.frame(x = c(0, 1)), aes(x)) + 
+  stat_function(fun = function(x) exp(-0.5*E*x)-1, aes(linetype = "E/2", color = "E/2"), alpha = 0.5) +
+  stat_function(fun = function(x) exp(-E*x)-1, aes(linetype = "E", color = "E")) +
+  stat_function(fun = function(x) exp(-2.0*E*x)-1, aes(linetype = "2E", color = "2E"), alpha = 0.5) +
+  geom_vline(xintercept = 0, linetype="dotted") +
+  geom_hline(yintercept = 0, linetype="dotted") +
+  #annotate("text", x=-0.4, y=-.25, label = paste("Constant elasticity =",E)) + #, family = "Times New Roman") +
+  scale_x_continuous(expression(Delta~"Price"), labels = scales::percent, limits = c(-1,1)) +
+  scale_y_continuous(expression(Delta~"Demand"), labels = scales::percent, limits = c(-0.5,0.5)) +
+  scale_color_brewer("Elasticity", palette = "Set1", limits = c("E","E/2","2E"), labels = elabs) +
+  scale_linetype("Elasticity", limits = c("E","E/2","2E"), labels = elabs) +
+  theme_classic()
+# theme(text=element_text(family="Times New Roman"))
+# plot.elasticity
+
+#### Pricing funcition 
 plot.price <- ggplot(data.frame(x = c(0, 40)), aes(x)) + 
   stat_function(fun = fun.price, args = list(pmin, pmax, a, b)) +
   scale_y_continuous("Price [$/veh]", labels = scales::dollar, breaks = seq(0,15,by=2), limits = c(0,pmax)) +
@@ -126,20 +149,10 @@ plot.price <- ggplot(data.frame(x = c(0, 40)), aes(x)) +
 # theme(text=element_text(family="Times New Roman"))
 # plot.price
 
-plot.elasticity <- ggplot(data.frame(x = c(0, 1)), aes(x)) + 
-  stat_function(fun = function(x) exp(-E*x)-1) +
-  geom_vline(xintercept = 0, linetype="dotted") +
-  geom_hline(yintercept = 0, linetype="dotted") +
-  annotate("text", x=-0.4, y=-.25, label = paste("Constant elasticity =",E)) + #, family = "Times New Roman") +
-  scale_x_continuous(expression(Delta~"Price"), labels = scales::percent, limits = c(-1,1)) +
-  scale_y_continuous(expression(Delta~"Demand"), labels = scales::percent, limits = c(-0.5,0.5)) +
-  theme_classic()
-# theme(text=element_text(family="Times New Roman"))
-# plot.elasticity
 
-
+#### Demand Density Distributions (Same as flow, but with density)
 plot.demanddensity <- ggplot(dat) + 
-  geom_line(aes(x=time,y=k1, linetype="Flat")) + 
+  geom_line(aes(x=time,y=k1, linetype="Fixed")) + 
   geom_line(aes(x=time,y=k2, linetype="Dynamic")) + 
   scale_y_continuous("Traffic density [veh/km/ln]") +
   scale_linetype("Tolling scheme") +
@@ -151,9 +164,9 @@ plot.demanddensity <- ggplot(dat) +
         #text=element_text(family="Times New Roman"))
 # plot.demanddensity
 
-
+#### Demand flow distributions (Same as density, but with flow)
 plot.demandflow <- ggplot(dat) + 
-  geom_line(aes(x=time,y=q1, linetype="Flat")) + 
+  geom_line(aes(x=time,y=q1, linetype="Fixed")) + 
   geom_line(aes(x=time,y=q2, linetype="Dynamic")) + 
   scale_y_continuous("Traffic flow [veh/hr/lane]") +
   scale_linetype("Tolling scheme") +
@@ -165,8 +178,9 @@ plot.demandflow <- ggplot(dat) +
 # plot.demandflow
 
 
+#### Toll price distribution
 plot.toll <- ggplot(dat) +  
-  geom_line(aes(x=time,y=price1, linetype="Flat")) + 
+  geom_line(aes(x=time,y=price1, linetype="Fixed")) + 
   geom_line(aes(x=time,y=price2, linetype="Dynamic")) + 
   scale_y_continuous("Price [$/veh]", labels = scales::dollar, breaks = seq(0,pmax,by=2), limits = c(0,pmax)) +
   scale_linetype("Tolling scheme") +
@@ -178,7 +192,7 @@ plot.toll <- ggplot(dat) +
         #text=element_text(family="Times New Roman"))
 # plot.toll
 
-
+#### Revenue distribution
 plot.revenue <- ggplot(dat) + 
   geom_line(aes(x=time,y=rev1, linetype=paste0("Fixed toll,\nTotal revenue: ", scales::dollar(sum(dat$rev1))))) +
   geom_line(aes(x=time,y=rev2, linetype=paste0("Dynamic toll,\nTotal revenue: ", scales::dollar(sum(dat$rev2))))) +
@@ -186,20 +200,20 @@ plot.revenue <- ggplot(dat) +
   scale_x_datetime("Time of day", labels = date_format("%l%p", tz='EST'), date_breaks = "3 hour",
                    limits = c(as.POSIXct('2000-01-01 00:00:00 EST', tz='EST'),
                               as.POSIXct('2000-01-01 24:00:00 EST', tz='EST'))) +
-  scale_linetype("Tolling scheme") +
+  scale_linetype("Tolling scheme:") +
   # annotate("text", x=as.POSIXct('2000-01-01 24:00:00 EST', tz='EST'), y=0.5*max(dat$rev2),
   #          label = paste0(paste0("Fixed toll: ", scales::dollar(sum(dat$rev1))),"\n",
   #                         paste0("Dynamic toll: ", scales::dollar(sum(dat$rev2)))), hjust = 0, size = 4) +
   theme_classic() +
-  theme(legend.position = "right",
+  theme(legend.position = "bottom",
         legend.background = element_blank())
         #text=element_text(family="Times New Roman"))
 # plot.revenue
 
 
-
+#### Cumulative revenue
 plot.revsum <- ggplot(dat) + 
-  geom_line(aes(x=time,y=sumrev1, linetype="Flat")) +
+  geom_line(aes(x=time,y=sumrev1, linetype="Fixed")) +
   geom_line(aes(x=time,y=sumrev2, linetype="Dynamic")) +
   scale_y_continuous("Total revenue [$]", labels = scales::dollar) +
   scale_linetype("Tolling scheme") +
@@ -212,6 +226,7 @@ plot.revsum <- ggplot(dat) +
 # plot.revsum
 
 
+#### Total revenue by Elasticity vs Pmax
 cuts = with(revmat, 
             c(seq(10*floor(10*min(revratio)), 100, length.out = 6)[-6],
               seq(100, 10*ceiling(10*max(revratio)), length.out = 7)))
@@ -225,13 +240,10 @@ plot.revmat <- ggplot(revmat, aes(x = E, y = surge)) +
                      labels = scales::percent_format()) +
   scale_fill_brewer(expression("Revenue Ratio\n(Dynamic vs Fixed)"),
                     palette = "RdBu", label = paste0(paste(round(cuts), round(cuts[-1]), sep = "-"),"%")) +
-  coord_cartesian(xlim = c(0, 1), ylim = c(1,3)) +
+  coord_cartesian(xlim = c(0, 2), ylim = c(1,3)) +
   theme_bw()
 # plot.revenue
 
-
-
-paste0(paste(round(cuts), round(cuts[-1]), sep = "-"),"%")
 
 
 # ggsave("../figures/toyprice.pdf", plot.price, device = cairo_pdf, width=4.25, height=2, units = "in")
